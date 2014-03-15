6d5dbd06c327cb84169aeb0fba74451ec7f5eb92
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-03-15 20:29:06 +0100

    debug

--- src/main/java/it/cnr/isti/hpc/erd/rest/RestService.java
+++ src/main/java/it/cnr/isti/hpc/erd/rest/RestService.java
@@ -43,7 +43,7 @@ public class RestService {
 
 	@POST
 	@Path("/shortTrac")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Consumes("application/x-www-form-urlencoded")
 	@Produces({ MediaType.TEXT_PLAIN })
 	public String annotatePost(Form form, @FormParam("runID") String runId,
 			@FormParam("TextID") String textId, @FormParam("Text") String text) {
