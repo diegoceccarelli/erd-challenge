61863607e51145a679685ef84f4bf2f25cde9be8
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-03-16 11:15:23 +0100

    fix post params

--- src/main/java/it/cnr/isti/hpc/erd/rest/RestService.java
+++ src/main/java/it/cnr/isti/hpc/erd/rest/RestService.java
@@ -21,13 +21,15 @@ import it.cnr.isti.hpc.erd.Annotator;
 
 import java.util.List;
 
-import javax.ws.rs.FormParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.multipart.FormDataParam;
 
 /**
  * @author Diego Ceccarelli <diego.ceccarelli@isti.cnr.it>
@@ -42,9 +44,12 @@ public class RestService {
 
 	@POST
 	@Path("/shortTrac")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Produces({ MediaType.TEXT_PLAIN })
-	public String annotatePost(@FormParam("runID") String runId,
-			@FormParam("TextID") String textId, @FormParam("Text") String text) {
+	public String annotatePost(@FormDataParam("runID") String runId,
+			@FormDataParam("TextID") String textId,
+			@FormDataParam("Text") String text) {
+
 		List<Annotation> annotations = annotator.annotate(runId, textId, text);
 
 		return encodeAnnotations(annotations);
